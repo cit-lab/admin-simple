d68f69aa7dce6a09ab5c83a8f9526aa6091c6f76
at15 <at15@outlook.com>
2015-07-24 22:44:54 +0800

    Add app folder
    for run the application

---
+++ app/server.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: at15
+ * Date: 15-7-24
+ * Time: 下午10:41
+ */
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+try{
+    $r = new Cit\Input\Route();
+    $app = new Cit\Application($r);
+    $app->run();
+}catch (Exception $e){
+    // TODO:handle all exceptions
+    die('oops');
+}
--- src/Application.php
+++ src/Application.php
@@ -9,11 +9,20 @@
 namespace Cit;
 
 use Cit\Input\Route;
+use Cit\Input\Router;
 
 class Application
 {
+    protected $router;
+
     public function __construct(Route $route)
     {
+        $this->router = new Router();
+        $this->router->applyRoute($route);
+    }
 
+    public function run()
+    {
+        
     }
 }
